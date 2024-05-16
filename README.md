070c3146854da9fbc794532c9e4fd85ba0b7272f
jayden <46307433+krmeet@users.noreply.github.com>
2024-05-16 23:49:26 +0100

    init from old repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# um
+> [!IMPORTANT]
+> hi
---
+++ client/scripts/Vulnus.gd
@@ -0,0 +1,13 @@
+extends Node
+
+func _ready():
+	DiscordRPC.app_id = 1239676558587723819
+	DiscordRPC.large_image = "icon"
+	DiscordRPC.large_image_text = "Vulnus Brasil"
+	DiscordRPC.details = "Getting ready to ball out"
+	DiscordRPC.state = "Loading"
+
+	DiscordRPC.refresh()
+
+func _process(delta):
+	DiscordRPC.run_callbacks()
