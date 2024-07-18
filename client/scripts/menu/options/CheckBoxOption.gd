e10a645961fc347cc7e69ce8eb29151224c8c7ec
jayden <46307433+kermeow@users.noreply.github.com>
2024-07-18 03:21:13 +0100

    fix options menu

--- client/scripts/menu/options/CheckBoxOption.gd
+++ client/scripts/menu/options/CheckBoxOption.gd
@@ -1,23 +1,27 @@
-#@tool
+@tool
 extends Control
-#
-#@export_category("Options")
-#@export var text:String:
-	#get: return $H/Label.text
-	#set(value): $H/Label.text = value
-#@export var tooltip:String:
-	#get: return tooltip_text
-	#set(value): tooltip_text = value
-#@export var target:String
-#@export var value:bool:
-	#get: return $H/CheckBox.button_pressed
-	#set(value): $H/CheckBox.button_pressed = value
-#
-#func _ready():
-	#if Engine.is_editor_hint(): return
-	#self.value = Vulnus.settings.get(target, false)
-#
-#func _update(value):
-	#if Engine.is_editor_hint(): return
-	#Vulnus.settings[target] = value
-	#Vulnus.save_settings()
+
+@export_category("Options")
+@export var text:String:
+	set(value):
+		text = value
+		if Engine.is_editor_hint(): $H/Label.text = value
+@export var tooltip:String:
+	get: return tooltip_text
+	set(value): tooltip_text = value
+@export var target:String
+@export var value:bool:
+	set(_value):
+		value = _value
+		if Engine.is_editor_hint(): $H/CheckBox.button_pressed = _value
+
+func _ready():
+	if Engine.is_editor_hint(): return
+	$H/Label.text = text
+	value = Vulnus.settings.get(target, false)
+	$H/CheckBox.button_pressed = value
+
+func _update(value):
+	if Engine.is_editor_hint(): return
+	Vulnus.settings[target] = value
+	Vulnus.save_settings()
